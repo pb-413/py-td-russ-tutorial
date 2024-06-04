b5692b945cb082294ae7b45db3f1a25089b4f737
Patrick K. Byers <pbyers413@gmail.com>
2024-06-03 20:43:28 -0400

    Tutorial Part 2
    https://youtu.be/D9U5AEpt9xc?si=Qw6CXBkUo5Ooyzr0

--- src/py_td_russ_tutorial/main.py
+++ src/py_td_russ_tutorial/main.py
@@ -3,6 +3,7 @@
 import pygame as pg
 
 import constants as c
+from enemy import Enemy
 
 
 # Initialize Pygame.
@@ -15,16 +16,36 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
 pg.display.set_caption("Tower Defense")
 
+# Load images.
+enemy_image = pg.image.load('assets/images/enemies/enemy_1.png').convert_alpha()
+
+# Create groups.
+enemy_group = pg.sprite.Group()
+
+enemy = Enemy((200, 300), enemy_image)
+enemy_group.add(enemy)
+
 # Game loop.
 run = True
 while run:
 
     clock.tick(c.FPS)
 
+    screen.fill("grey100")
+
+    # Update groups.
+    enemy_group.update()
+
+    # Draw groups.
+    enemy_group.draw(screen)
+
     # Event handler.
     for event in pg.event.get():
         # Quit program.
         if event.type == pg.QUIT:
             run = False
+
+    # Update display.
+    pg.display.flip()
 
 pg.quit()
